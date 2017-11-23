a0c109821530cfa06daed4f92d14bb655e16c7c8
Xavier Arques <xavierarques@yahoo.com>
2017-11-23 17:00:58 +0100

    Optim

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -12,14 +12,16 @@ class DosesController < ApplicationController
 
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
-    @ingredient = Ingredient.find(params[:dose][:ingredient_id])
+    # @ingredient = Ingredient.find(params[:dose][:ingredient_id])
     @dose = Dose.new(doses_params)
     @dose.cocktail = @cocktail
-    @dose.ingredient = @ingredient
+    # @dose.ingredient = @ingredient
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
-      render :new
+      render 'cocktails/show'
+      # Optimization
+      # render :new
     end
   end
 
@@ -29,6 +31,6 @@ class DosesController < ApplicationController
   end
 
   def doses_params
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
   root to: "cocktails#index"
   resources :cocktails, only: [:index, :show, :new, :create, :destroy]  do
-    resources :doses, only: [:new, :create]
+    resources :doses, only: [:create]
+    # resources :doses, only: [:new]
   end
   resources :doses, only: [:destroy]
-
-  # resources :restaurants, only: [:index, :show, :new, :create] do
-  #   resources :reviews, only: [ :index, :new, :create ]
-  # end
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
